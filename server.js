7da71f999842bd6026c5b4ea01c44db77f56a762
ClinicPro213 <moatazalkhawlani1@gmail.com>
2026-03-24 08:57:20 +0300

    Update MongoDB connection URI environment variable

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
